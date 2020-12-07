c414a94d65f7e1759b06fa65afa8626e8fc5bb50
Oussemax1 <amaroussema@gmail.com>
2020-12-07 15:47:54 +0100

    api checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import React,{ useState, useEffect } from 'react';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from "axios";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const [data, setData] = useState([]);
+ useEffect(() => {
+   const fetchData = async () => {
+     const listOfUSer  = await axios.get(
+       "https://jsonplaceholder.typicode.com/users"
+     );
+     setData(listOfUSer.data);
+   };
+   fetchData();
+ }, []);
+ return (
+ 	<div className="text-center mx-auto w-25 my-5 border border-info alert-warning">
+ 	<h3 className="alert alert-danger m-2  " > <em>My Invite List</em> </h3>
+   <ul className="list-unstyled">
+     {data.map(item => (
+       <li key={item.id}  className=" alert alert-success m-2 py-1">
+         <h4>{item.name}</h4>
+       </li>
+     ))}
+   </ul>
+   </div>
+ );
 }
-
 export default App;
